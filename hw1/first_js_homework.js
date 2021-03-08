b74ff4ff9c764cba9d2f1e74d5ccd3da263bb59f
jcari-rgb <582119jc@gmail.com>
2021-03-08 17:08:32 -0500

    section 1

---
+++ hw1/first_js_homework.js
@@ -0,0 +1,11 @@
+//1. What does DRY stand for? The acronym DRY in code stands for "Don't
+// Repeat Yourself". It is important to pay attention and remember it
+// because, it makes suggests that we shouldn't write repetitive code 
+// which is poor practice. Also, one of the tools that we have learned
+// that can help us write DRY code, are functions. 
+
+
+//2. What is the difference between Var, Const and Let? Const and Let are
+// block-scoped, while Var isn't. Also, Const values can't be reassined, while
+// Let can be. Also Let var can't be declared again, while Var can be decla-
+//red again. 
